e49a3d07eb80e26fe9a8b841615b346b505aa272
Ihor Klimov <iklimov6@gmail.com>
2017-07-21 07:51:19 +0100

    Added app

---
+++ app/src/main/java/com/myhexaville/topdemo/FilterActivity.java
@@ -0,0 +1,46 @@
+package com.myhexaville.topdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class FilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_second, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            setResult(RESULT_CANCELED);
+            onBackPressed();
+        } else if (item.getItemId() == R.id.action_done) {
+            setResult(RESULT_OK, new Intent().putExtra("sortBy", "name"));
+            onBackPressed();
+        }
+        return true;
+    }
+
+    public void goBack(View view) {
+        setResult(RESULT_CANCELED);
+        onBackPressed();
+    }
+}
---
+++ app/src/main/java/com/myhexaville/topdemo/MainActivity.java
@@ -0,0 +1,54 @@
+package com.myhexaville.topdemo;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_filter) {
+            startActivityForResult(new Intent(this, FilterActivity.class),
+                    100,
+                    ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+        }
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                Log.d(LOG_TAG, "onActivityResult: sortBy "+ data.getStringExtra("sortBy"));
+            } else {
+                Log.d(LOG_TAG, "onActivityResult: canceled");
+            }
+        }
+    }
+}
